a7c15c49c2aae9c55f08a6d33f02a24f98872e73
Pablo Miguel <pablo2003miguel0903@gmail.com>
2024-04-05 17:20:49 -0300

    implementando seção

--- Teste 1/js/script.js
+++ Teste 1/js/script.js
@@ -1,3 +1,23 @@
 //1 - Movendo-se pelo DOM
 console.log(document.body);
+console.log(document.body.childNodes[1]);
+console.log(document.body.childNodes[1].childNodes);
+console.log(document.body.childNodes[1].childNodes[1].textContent);
 
+
+//2 - selecionando por tag
+const listItens = document.getElementsByTagName("li");
+console.log(listItens);
+
+
+//3 - selecionando elemento por id
+const title = document.getElementById("title");
+console.log(title)
+
+
+//4 - selecionando elementos por classe
+const products = document.getElementsByClassName("product");
+console.log(products);
+
+
+//5 - 
