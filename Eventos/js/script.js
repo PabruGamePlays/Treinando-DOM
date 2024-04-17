f33bddf0d5219a66fcfb2b47ccb89d4e66813de7
Pablo Miguel <pablo2003miguel0903@gmail.com>
2024-04-17 17:38:38 -0300

    mais informações

--- Eventos/js/script.js
+++ Eventos/js/script.js
@@ -1,6 +1,5 @@
 //1 - adicionando eventos
 const btn = document.querySelector("#my-button");
-
 btn.addEventListener("click", function(){
     console.log("clicou aqui")
 });
@@ -8,14 +7,11 @@ btn.addEventListener("click", function(){
 
 //2 - removendo eventos
 const secondBtn = document.querySelector("#btn");
-
 function imprimirMensagem(){
     console.log("Teste")
 }
-
 secondBtn.addEventListener("click", imprimirMensagem);
 const thirdBtn = document.querySelector("#other-btn");
-
 thirdBtn.addEventListener("click", () => {
     console.log("Evento removido");
     secondBtn.removeEventListener("click", imprimirMensagem);
@@ -24,10 +20,57 @@ thirdBtn.addEventListener("click", () => {
 
 //3 - argumento do evento
 const myTitle = document.querySelector("#my-title");
-
 myTitle.addEventListener("click", (event) => {
     console.log(event);
     console.log(event.offsetX);
     console.log(event.pointerType);
     console.log(event.target);
 });
+
+
+//4 - Propagação
+const containerBtn = document.querySelector("#btn-container");
+const btnInsiderContainer = document.querySelector("#div-btn");
+containerBtn.addEventListener("click", () => {
+    console.log("Evento 1")
+});
+btnInsiderContainer.addEventListener("click", (e) => {
+    e.stopPropagation();
+    console.log("Evento 2")
+});
+
+
+//5 - removendo eventro padrão
+const a = document.querySelector("a");
+a.addEventListener("click", (e) => {
+    e.preventDefault()
+    console.log("Não alterou a página");
+});
+
+
+//6 - eventos de tecla
+document.addEventListener("keyup", (e) => {
+    console.log(`Soltou a tecla ${e.key}`);
+});
+document.addEventListener("keydown", (e) => {
+    console.log(`Apertou a tecla ${e.key}`);
+});
+
+
+//7 - eventos de mouse
+const mouseEvents = document.querySelector("#mouse");
+mouseEvents.addEventListener("mousedown", () => {
+    console.log("Pressionou o botão");
+});
+mouseEvents.addEventListener("mouseup", () => {
+    console.log("Soltou o botão");
+});
+mouseEvents.addEventListener("dblclick", () => {
+    console.log("Clique duplo");
+});
+
+
+//8 - movimento do mouse
+// document.addEventListener("mousemove", (e) => {
+//     console.log(`No eixo X: ${e.x} e no eixo Y: ${e.y}`)
+// })
